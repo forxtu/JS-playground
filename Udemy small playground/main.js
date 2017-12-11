8fcd8dca92d7862fc7079724492ad529d296aed5
forxtu <forxtu@gmail.com>
2017-12-11 16:25:31 +0100

    feat(udemy): init playground

---
+++ Udemy small playground/main.js
@@ -0,0 +1,7 @@
+var ourput = document.querySelector('.output');
+
+var arr = ['Den', 'Kevin', 'Peter'];
+
+var userName = prompt("Tell me if user exist");
+
+alert((arr.indexOf(userName) > -1) ? "Exist" : "No");

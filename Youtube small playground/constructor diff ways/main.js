ef8c839250cf0ee2f9be46a2e18c50e7e876643e
forxtu <forxtu@gmail.com>
2018-01-05 15:26:24 +0100

    feat(classic constructor): inheritance

--- Youtube small playground/constructor diff ways/main.js
+++ Youtube small playground/constructor diff ways/main.js
@@ -1,6 +1,8 @@
+/*****************************/
 /* Object.create constructor */
+/*****************************/
 var Person = {
-    constructor: function(name, age, sex){
+    constructor: function(name, age, sex) {
         this.name = name;
         this.age = age;
         this.sex = sex;
@@ -8,51 +10,65 @@ var Person = {
     },
 
     greet: function() {
+        console.log('*Obj.create()');
         console.log(this.name + ' ' + this.age + ' ' + this.sex);
     }
-}
+};
 
-var user,
-    user2;
-
+var user;
 user = Object.create(Person).constructor('Dennis', 24, 'male');
-user2 = Object.create(Person).constructor('Hanna', 23, 'female');
 user.greet();
-user2.greet();
 
-/* extends object */
-var developer = Object.create(Person);
-developer.constructor = function(name, age, sex, skills) {
+/* inheritance object */
+var DeveloperObj = Object.create(Person);
+DeveloperObj.constructor = function(name, age, sex, skills) {
     Person.constructor.apply(this, arguments);
     this.skills = skills;
     return this;
 }
-var webDeveloper = Object.create(developer).constructor('Web dev Den', 24, 'male', ['html', 'css', 'js']);
+var webDeveloper = Object.create(DeveloperObj).constructor('Web dev Den', 24, 'male', ['html', 'css', 'js']);
 console.log(webDeveloper.skills);
 webDeveloper.develop = function() {
     console.log('Work!');
 }
 webDeveloper.develop();
 
-
-/* es5 function constructor */
+/************************************/
+/* es5 classic function constructor */
+/************************************/
 var Person2 = function(name, age, sex) {
     this.name = name;
     this.age = age;
     this.sex = sex;
 
     this.greet2 = function() {
-        // console.log(this.name + ' ' + this.age + ' ' + this.sex);
+        console.log('*classic constructor');
+        console.log(this.name + ' ' + this.age + ' ' + this.sex);
     }
-}
+};
 
 var person = new Person2('Jhon', 34, 'male');
-var person2 = new Person2('Alice', 23, 'female');
-
 person.greet2();
-person2.greet2();
 
-/* es6 class */
+/* inheritance prototype */
+var Developer = function(name, age, sex, skills) {
+    Person2.apply(this, arguments);
+    this.skills = skills || [];
+
+    this.info = function() {
+        console.log('Hey ' + this.name + '! ' + 'Your skills: ' + this.skills);
+    }
+};
+// Developer.prototype = Object.create(Person2.prototype); // but we have Person2 constructor
+// Developer.prototype.constructor = Developer; // now we have Developer constructor
+var webDeveloper2 = new Developer('Andy', 45, 'male', ['php', 'vuejs']);
+webDeveloper2.info();
+console.log(webDeveloper2 instanceof Developer); // check if webDeveloper2 exemplar of class Developer - true
+console.log(webDeveloper2 instanceof Person2); // check if webDeveloper2 exemplar of class Person - false if we don't use row #62
+
+/****************************/
+/*        es6 class         */
+/****************************/
 class Person3 {
     constructor(name, age, sex){
         this.name = name;
@@ -61,7 +77,8 @@ class Person3 {
     }
 
     greet3() {
-        // console.log(this.name + ' ' + this.age + ' ' + this.sex);
+        console.log('*ES6');
+        console.log(this.name + ' ' + this.age + ' ' + this.sex);
     }
 }
 
